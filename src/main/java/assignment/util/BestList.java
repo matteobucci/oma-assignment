4182bbbfc611ab97c2fc0af73b5bcf5f593ee091
matteobucci <matteo.bucci.1995@gmail.com>
2018-01-07 21:45:27 +0100

    New final euristic

---
+++ src/main/java/assignment/util/BestList.java
@@ -0,0 +1,35 @@
+package main.java.assignment.util;
+
+import java.util.LinkedList;
+
+public class LimitedQueue<E> extends LinkedList<E> {
+
+    private int limit;
+
+    public LimitedQueue(int limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public boolean add(E o) {
+        boolean added = super.add(o);
+        while (added && size() > limit) {
+            super.remove();
+        }
+        return added;
+    }
+
+    public void newSize(int i) {
+        if(i < limit) {
+            for(i=limit; i<this.size(); i++){
+                this.remove(i);
+            }
+        }
+
+        limit = i;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+}
---
+++ src/main/java/assignment/util/ExamPair.java
@@ -0,0 +1,4 @@
+package main.java.assignment.util;
+
+public class ExamPair {
+}
